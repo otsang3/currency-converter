18c7c5c51f9dce311c455a04e59fd76741d45569
otsang3 <ohhtsang@gmail.com>
2020-09-06 19:13:26 +0100

    minor css changes

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,8 +6,8 @@ function Result(props) {
         <div className="col-result">
             <h3 style={{marginTop: "1em"}}>{props.state.amount} {props.state.resultCurrency1} =</h3>
             <h1>{props.state.result.toFixed(6)} <span style={{fontSize: "0.5em"}}>{props.state.resultCurrency2}</span></h1>
-            <h3>1 {props.state.resultCurrency1} = {props.state.homeRate.toFixed(6)}</h3>
-            <h3>1 {props.state.resultCurrency2} = {props.state.awayRate.toFixed(6)}</h3>
+            <h4>1 {props.state.resultCurrency1} = {props.state.homeRate.toFixed(6)}</h4>
+            <h4>1 {props.state.resultCurrency2} = {props.state.awayRate.toFixed(6)}</h4>
         </div>      
     )
 }
