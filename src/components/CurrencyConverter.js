ea98d1e84776aa7298bef33cd4cda99b779fb5c7
otsang3 <ohhtsang@gmail.com>
2020-09-06 13:31:18 +0100

    set up state and initial return for CurrencyConverter.js

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { currencies } from '../data/Currencies';
+import CurrencyList from './CurrencyList';
 
 function CurrencyConverter() {
 
+    const initialState = {
+        amount: 0,
+        currencies: [currencies],
+        selectedCurrency1: null,
+        selectedCurrency2: null,
+        
+    }
+
+    const [state, setState] = useState(initialState)
+
     return(
         <div>
-
+            <input/>
         </div>
     )
 }
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CurrencyConverter from './CurrencyConverter';
 
 function MainContainer() {
 
     return(
         <div>
-
+            <CurrencyConverter/>
         </div>
     )
 }
